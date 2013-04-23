00d443b6a78b914cba6804dbd6194ac0ebe30073
'Paul <thesamuraipanda@gmail.com>
2013-04-23 13:48:08 -0400

    Added a couple seekbars and a button. Not functional yet.

--- BadMetronome/src/com/example/badmetronome/MainActivity.java
+++ BadMetronome/src/com/example/badmetronome/MainActivity.java
@@ -3,13 +3,36 @@ package com.example.badmetronome;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.SeekBar.OnSeekBarChangeListener;
 
 public class MainActivity extends Activity {
-
+	
+	SeekBar speedBar, accuracyBar;
+	Button playPause;
+	int speed, accuracy;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		speedBar = (SeekBar) this.findViewById(R.id.speedBar);
+		accuracyBar = (SeekBar) this.findViewById(R.id.accuracyBar);
+		playPause = (Button) this.findViewById(R.id.button1);
+		
+		speed = speedBar.getProgress();
+		accuracy = accuracyBar.getProgress();
+		
+		playPause.setOnClickListener(new Button.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				
+			}
+		});
+
 	}
 
 	@Override
